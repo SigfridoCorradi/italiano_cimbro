c985905e894d0c7da6ae0c9b228d6bc343e3bf4e
Sigfrido Corradi <78537708+SigfridoCorradi@users.noreply.github.com>
2025-03-24 14:20:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,30 +3,7 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-translator_istance = translator.Translator(
-    pretrained_model_name = "Helsinki-NLP/opus-mt-it-de", 
-    pretrained_download = True, 
-    pretrained_local_dir_download = './pretrained_model', 
-    
-    dataset_training_csv = "training_dataset.csv", 
-    dataset_evaluation_csv = "evaluation_dataset.csv", 
-    dataset_csv_delimiter = "#", 
-    dataset_source_csv_column_header = "source", 
-    dataset_target_csv_column_header = "target", 
-    use_metric_as_trainer_callback = True,
-
-    optimizer_learning_rate = [1e-5, 5e-4],
-    optimizer_per_device_train_batch_size = [6, 8, 32],
-    optimizer_num_train_epochs = [4, 6],
-    optimizer_weight_decay = [1e-4, 1e-2],
-
-    fine_tuned_model_dir = "./fine_tuned_model",
-    fine_tuning_learning_rate=8e-5,
-    fine_tuning_per_device_train_batch_size=8,
-    fine_tuning_per_device_eval_batch_size=8,
-    fine_tuning_num_train_epochs=8,
-    fine_tuning_weight_decay=0.01
-)
+translator_istance = translator.Translator(fine_tuned_model_dir = "./fine_tuned_model")
 
 @app.route('/')
 def home():
